20a7f082ac2f47e64a7f1df6fe54dffd053a59af
ntnikhilesh <ntnikhilesh@gmail.com>
2018-04-30 00:26:29 +0530

    using regex

--- src/app/helloTest.spec.ts
+++ src/app/helloTest.spec.ts
@@ -1,31 +1,42 @@
 describe('hello test',()=>{
 
-
+//1
     // it('check if hello test is hello',()=>{
     //     expect('hello').toBe('hello')
     // })
 
-
+//2
     // it('check if hello test is not hello',()=>{
     //     expect('hello').not.toBe('hello1')
     // })
 
-
+//3
     //using varibale
     //will intialize variable inside method called beforeEach
 
-    let expected='';
+    // let expected='';
+    // beforeEach(()=>{
+    //     expected='hello'
+    // })
+
+    // //cleaning variable
+    // afterEach(()=>{
+    //     expected='';
+    // })
+
+    // it('check if hello test is hello',()=>{
+    //     expect('hello').toBe(expected)
+    // })
+
+//4
+    //using regex
+    let expectMatch=null; //bx regex will be object not string thats y null instead of ''
+
     beforeEach(()=>{
-        expected='hello'
+        expectMatch=new RegExp(/^hello/);
     })
 
-    //cleaning variable
-    afterEach(()=>{
-        expected='';
+      it('check if hello test is hello using regex',()=>{
+        expect('hello').toMatch(expectMatch);
     })
-
-    it('check if hello test is hello',()=>{
-        expect('hello').toBe(expected)
-    })
-
 })
