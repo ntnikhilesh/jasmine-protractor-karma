891ae969811dede0f81b5fc72343368df4ee7673
ntnikhilesh <ntnikhilesh@gmail.com>
2018-04-30 00:13:34 +0530

    check if hello test is not hello

--- src/app/helloTest.spec.ts
+++ src/app/helloTest.spec.ts
@@ -1,6 +1,13 @@
 describe('hello test',()=>{
-    it('check if hello test is hello',()=>{
-        expect('hello').toBe('hello')
+
+
+    // it('check if hello test is hello',()=>{
+    //     expect('hello').toBe('hello')
+    // })
+
+
+    it('check if hello test is not hello',()=>{
+        expect('hello').not.toBe('hello1')
     })
 
 })
