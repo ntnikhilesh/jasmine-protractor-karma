c6884c2e066f967189e970eaabe2e160eacd2be4
ntnikhilesh <ntnikhilesh@gmail.com>
2018-04-30 01:28:14 +0530

    e2e - should navigate home page

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { browser, by, element } from 'protractor';
 
 describe('jasmine-protractor-karma App', () => {
   let page: AppPage;
@@ -7,8 +8,12 @@ describe('jasmine-protractor-karma App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  // it('should display welcome message', () => {
+  //   page.navigateTo();
+  //   expect(page.getParagraphText()).toEqual('Welcome to app!');
+  // });
+
+  it('should navigate home page', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
