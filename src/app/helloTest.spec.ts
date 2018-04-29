171cc5e51a5c054a6b112938702a88e5e02edeac
ntnikhilesh <ntnikhilesh@gmail.com>
2018-04-30 00:18:24 +0530

    using variable

--- src/app/helloTest.spec.ts
+++ src/app/helloTest.spec.ts
@@ -6,8 +6,21 @@ describe('hello test',()=>{
     // })
 
 
-    it('check if hello test is not hello',()=>{
-        expect('hello').not.toBe('hello1')
+    // it('check if hello test is not hello',()=>{
+    //     expect('hello').not.toBe('hello1')
+    // })
+
+
+    //using varibale
+    //will intialize variable inside method called beforeEach
+
+    let expected='';
+    beforeEach(()=>{
+        expected='hello'
+    })
+
+    it('check if hello test is hello',()=>{
+        expect('hello').toBe(expected)
     })
 
 })
